04ab52f55b048ab5d8fa2e03654d686b3326649d
jn8416 <71154756+jn8416@users.noreply.github.com>
2021-02-24 16:00:09 -0500

    Add files via upload

--- quotes/quotes.py
+++ quotes/quotes.py
@@ -57,7 +57,7 @@ class Quotes(commands.Cog):
 		"""
 		items = [escape(c, mass_mentions=True) for c in items]
 		if len(items) == 2:
-			channel = ctx.guild.get_channel(await self.config.guild(ctx.guild).quoteset_channel())
+			channel = ctx.author.guild.get_channel(await self.config.guild(ctx.author.guild).quoteset_channel())
 			if channel is None:
 				channel = guild.system_channel
 			content = items[0]
