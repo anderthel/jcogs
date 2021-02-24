79f16463ce44c0c7a782ba53d075c3b6631bdfc5
jn8416 <71154756+jn8416@users.noreply.github.com>
2021-02-24 16:31:07 -0500

    Test 4

--- quotes/quotes.py
+++ quotes/quotes.py
@@ -47,7 +47,7 @@ class Quotes(commands.Cog):
 
 	@quote.command(name="create")
 	@allowed_to_create()
-	async def quote_create(self, message):
+	async def quote_create(self, ctx: message, *items):
 		"""
 		If a minimum required role has been set, users must have that role or
 		higher, be in the mod/admin role, or be the guild owner in order to use this command
@@ -55,21 +55,21 @@ class Quotes(commands.Cog):
 		The quote will only be created if all information is provided properly.
 		Porper format is Double quotes surrounding quote followed by double quotes surrounding where its from/who its by
 		"""
-		#items = [escape(c, mass_mentions=True) for c in message]
-		#if len(message) == 2:
-		#	channel = await self.config.guild(message.guild).channel()
-		#	if channel is None:
-		#		channel = guild.system_channel
-		#	content = items[0]
-		#	byfrom = items[1]
-		#	poster = ctx.author
-		#	embed=discord.Embed(title=content, description=byfrom)
-		#	embed.set_footer(text=poster)
-		#	await channel.send(embed=embed)
-		await message.send(message)
+		items = [escape(c, mass_mentions=True) for c in message]
+		if len(items) == 2:
+			channel = await self.config.guild(message.guild).channel()
+			if channel is None:
+				channel = guild.system_channel
+			content = items[0]
+			byfrom = items[1]
+			poster = ctx.author
+			embed=discord.Embed(title=content, description=byfrom)
+			embed.set_footer(text=poster)
+			await channel.send(embed=embed)
+		
 
-		#else:
-		#	await ctx.send("Not properly formatted. Porper format is double quotes surrounding quote followed by double quotes surrounding where its from/who its by")
+		else:
+			await ctx.send("Not properly formatted. Porper format is double quotes surrounding quote followed by double quotes surrounding where its from/who its by")
 
 	@commands.group()
 	@checks.admin_or_permissions(manage_guild=True)
