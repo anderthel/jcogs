c9dbae337034529b64b9def49677d43b93917e40
jn8416 <71154756+jn8416@users.noreply.github.com>
2021-02-24 21:04:22 -0500

    Add files via upload

--- quotes/quotes.py
+++ quotes/quotes.py
@@ -20,7 +20,7 @@ class Quotes(commands.Cog):
 		#define quote
 		sentence = content
 		#set font
-		fnt = ImageFont.truetype('font.ttf', 40)
+		fnt = ImageFont.truetype('/data/cogs/CogManager/cogs/quotes/font.ttf', 40)
 		# color is the backgroundColor of the Image
 		img = Image.new('RGB', (x1, y1), color = (0,0,0))
 		d = ImageDraw.Draw(img)
